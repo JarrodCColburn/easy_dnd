63e5305451e0a64b974c086cbd2cedca0f257d4f
strangerweather <strangerweather@gmail.com>
2018-12-31 12:53:58 +0000

    introduced scoped model

--- lib/cards/card_1.dart
+++ lib/cards/card_1.dart
@@ -1,7 +1,6 @@
-import 'dart:async';
-
+import 'package:easy_dnd/model.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'package:scoped_model/scoped_model.dart';
 
 class CardOne extends StatefulWidget {
   @override
@@ -11,89 +10,147 @@ class CardOne extends StatefulWidget {
 }
 
 class _CardOneState extends State<CardOne> {
-  final _platformChannel1 =
-      MethodChannel('strangerweather.com/easy_dnd/receiver');
-  final _platformChannel2 = EventChannel('strangerweather.com/easy_dnd/stream');
   bool pressed = false;
-  final controller = StreamController<String>();
-  Stream<String> _statusStream;
-
-  _CardOneState() {
-      _statusStream = controller.stream;
-  }
-
-  Future<Null> _dndOn() async {
-    await _platformChannel1.invokeMethod('ON');
-  }
-
-  Future<Null> _dndOff() async {
-    await _platformChannel1.invokeMethod('OFF');
-
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    controller.close();
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    _platformChannel2
-        .receiveBroadcastStream()
-        .listen(_onEvent, onError: _onError);
-  }
-
-  void _onEvent(Object event) {
-    print(event);
-    controller.sink.add("$event");
-  }
-
-  void _onError(Object error) {
-    controller.sink.add("Error");
-  }
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      color: Color.fromARGB(255, 76, 175, 80),
-      elevation: 5.0,
-      child: InkWell(
-        splashColor: Colors.blueGrey,
-        onTap: () {
-          setState(() {
-            pressed = !pressed;
-          });
-          pressed ? _dndOn() : _dndOff();
-        },
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Padding(
-                padding: EdgeInsets.only(left: 10.0, right: 10.0),
-                child: StreamBuilder<String>(
-                  stream: _statusStream,
-                  builder: (context, snapshot) {
-                    if (snapshot.hasData) {}
-                    return Text(
-                      "${snapshot.data}",
-                      style: TextStyle(color: Colors.white, fontSize: 20.0),
-                      textAlign: TextAlign.center,
-                    );
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.blueGrey),
+      home: ScopedModel<AppModel>(
+        model: AppModel(),
+        child: ScopedModelDescendant<AppModel>(
+          builder: (context, child, model) => Card(
+                color: Color.fromARGB(255, 76, 175, 80),
+                elevation: 5.0,
+                child: InkWell(
+                  splashColor: Colors.blueGrey,
+                  onTap: () {
+                    setState(() {
+                      pressed = !pressed;
+                    });
+                    pressed ? model.dndOn() : model.dndOff();
                   },
                 ),
               ),
-            ],
-          ),
         ),
       ),
     );
   }
 }
-
-
-
-
+//
+//class _CardOneState extends State<CardOne> {
+//  final _platformChannel2 = EventChannel('strangerweather.com/easy_dnd/stream');
+//  final controller = StreamController<String>();
+//  Stream<String> _statusStream;
+//  bool pressed = false;
+//
+//  _CardOneState() {
+//    _statusStream = controller.stream;
+//  }
+//
+//  @override
+//  void dispose() {
+//    super.dispose();
+//    controller.close();
+//  }
+//
+//  @override
+//  void initState() {
+//    super.initState();
+//    _platformChannel2
+//        .receiveBroadcastStream()
+//        .listen(_onEvent, onError: _onError);
+//  }
+//
+//  void _onEvent(Object event) {
+//    print(event);
+//    controller.sink.add("$event");
+//  }
+//
+//  void _onError(Object error) {
+//    controller.sink.add("Error");
+//  }
+//
+//  @override
+//  Widget build(BuildContext context) {
+//    return ScopedModel<AppModel>(
+//      model: AppModel(),
+//child: ScopedModelDescendant<Model>(
+//  builder: (context, child, model) => Card(
+//    color: Color.fromARGB(255, 76, 175, 80),
+//    elevation: 5.0,
+//    child: InkWell(
+//      splashColor: Colors.blueGrey,
+//
+//      child: Center(
+//        child: Column(
+//          mainAxisAlignment: MainAxisAlignment.center,
+//          crossAxisAlignment: CrossAxisAlignment.center,
+////            children: <Widget>[
+////              Padding(
+////                padding: EdgeInsets.only(left: 10.0, right: 10.0),
+////                child: StreamBuilder<String>(
+////                  stream: _statusStream,
+////                  builder: (context, snapshot) {
+////                    if (snapshot.hasData) {}
+////                    return Text(
+////                      "${snapshot.data}",
+////                      style: TextStyle(color: Colors.white, fontSize: 20.0),
+////                      textAlign: TextAlign.center,
+////                    );
+////                  },
+////                ),
+////              ),
+////            ],
+////        ),
+////      ),
+////,
+////  ),
+////),
+////
+////    );
+////  }
+//
+//
+////      child: Card(
+////        color: Color.fromARGB(255, 76, 175, 80),
+////        elevation: 5.0,
+////        child: InkWell(
+////          splashColor: Colors.blueGrey,
+////          onTap: () {
+////            setState(
+////              () {
+////                pressed = !pressed;
+////              },
+////            );
+////            ScopedModelDescendant<AppModel>(
+////              builder: (context, child, model) {
+////                pressed ? model.dndOn() : model.dndOff();
+////              },
+////            );
+////          },
+////          child: Center(
+////            child: Column(
+////              mainAxisAlignment: MainAxisAlignment.center,
+////              crossAxisAlignment: CrossAxisAlignment.center,
+////              children: <Widget>[
+////                Padding(
+////                  padding: EdgeInsets.only(left: 10.0, right: 10.0),
+////                  child: StreamBuilder<String>(
+////                    stream: _statusStream,
+////                    builder: (context, snapshot) {
+////                      if (snapshot.hasData) {}
+////                      return Text(
+////                        "${snapshot.data}",
+////                        style: TextStyle(color: Colors.white, fontSize: 20.0),
+////                        textAlign: TextAlign.center,
+////                      );
+////                    },
+////                  ),
+////                ),
+////              ],
+////            ),
+////          ),
+////        ),
+////      ),
--- lib/main.dart
+++ lib/main.dart
@@ -15,48 +15,49 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(primarySwatch: Colors.blueGrey),
-        home: Scaffold(
-          appBar: AppBar(
-            title: Text('Easy DND'),
-          ),
-          body: GridView.count(
-            crossAxisCount: 2,
-            childAspectRatio: 1.0,
-            padding: const EdgeInsets.all(15.0),
-            mainAxisSpacing: 4.0,
-            crossAxisSpacing: 4.0,
-            children: <Widget>[
-              CardOne(),
-              CardTwo(),
-              Example(
-                text: 'Block Priority',
-                color: Color.fromARGB(255, 255, 87, 34),
-              ),
-              Example(
-                text: 'No Sound from Notifications',
-                color: Color.fromARGB(255, 255, 193, 7),
-              ),
-              Example(
-                text: 'Example 5',
-                color: Color.fromARGB(255, 63, 81, 181),
-              ),
-              Example(
-                text: 'Example 6',
-                color: Color.fromARGB(255, 156, 39, 176),
-              ),
-              Example(
-                text: 'Example 7',
-                color: Color.fromARGB(255, 233, 30, 99),
-              ),
-              Example(
-                text: 'Example 8',
-                color: Color.fromARGB(255, 121, 85, 72),
-              ),
-            ],
-          ),
-        ));
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.blueGrey),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Easy DND'),
+        ),
+        body: GridView.count(
+          crossAxisCount: 2,
+          childAspectRatio: 1.0,
+          padding: const EdgeInsets.all(15.0),
+          mainAxisSpacing: 4.0,
+          crossAxisSpacing: 4.0,
+          children: <Widget>[
+            CardOne(),
+            CardTwo(),
+            Example(
+              text: 'Block Priority',
+              color: Color.fromARGB(255, 255, 87, 34),
+            ),
+            Example(
+              text: 'No Sound from Notifications',
+              color: Color.fromARGB(255, 255, 193, 7),
+            ),
+            Example(
+              text: 'Example 5',
+              color: Color.fromARGB(255, 63, 81, 181),
+            ),
+            Example(
+              text: 'Example 6',
+              color: Color.fromARGB(255, 156, 39, 176),
+            ),
+            Example(
+              text: 'Example 7',
+              color: Color.fromARGB(255, 233, 30, 99),
+            ),
+            Example(
+              text: 'Example 8',
+              color: Color.fromARGB(255, 121, 85, 72),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
 
