91f352060d76f636fa2224e8952173b7a9232a14
strangerweather <strangerweather@gmail.com>
2018-12-31 13:57:31 +0000

    cleaned up card 1 and created stream_channel.dart

--- lib/cards/card_1.dart
+++ lib/cards/card_1.dart
@@ -1,6 +1,7 @@
 import 'package:easy_dnd/model.dart';
 import 'package:flutter/material.dart';
 import 'package:scoped_model/scoped_model.dart';
+import 'package:easy_dnd/stream_channel.dart';
 
 class CardOne extends StatefulWidget {
   @override
@@ -11,6 +12,7 @@ class CardOne extends StatefulWidget {
 
 class _CardOneState extends State<CardOne> {
   bool pressed = false;
+  StreamChannel streamChannel = new StreamChannel();
 
   @override
   Widget build(BuildContext context) {
@@ -20,7 +22,8 @@ class _CardOneState extends State<CardOne> {
       home: ScopedModel<AppModel>(
         model: AppModel(),
         child: ScopedModelDescendant<AppModel>(
-          builder: (context, child, model) => Card(
+          builder: (context, child, model) =>
+              Card(
                 color: Color.fromARGB(255, 76, 175, 80),
                 elevation: 5.0,
                 child: InkWell(
@@ -30,6 +33,18 @@ class _CardOneState extends State<CardOne> {
                       pressed = !pressed;
                     });
                     pressed ? model.dndOn() : model.dndOff();
+                    Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          Padding(
+                              padding: EdgeInsets.only(left: 10.0, right: 10.0),
+                              child:
+                          ),
+                        ],
+                      ),
+                    );
                   },
                 ),
               ),
@@ -38,119 +53,3 @@ class _CardOneState extends State<CardOne> {
     );
   }
 }
-//
-//class _CardOneState extends State<CardOne> {
-//  final _platformChannel2 = EventChannel('strangerweather.com/easy_dnd/stream');
-//  final controller = StreamController<String>();
-//  Stream<String> _statusStream;
-//  bool pressed = false;
-//
-//  _CardOneState() {
-//    _statusStream = controller.stream;
-//  }
-//
-//  @override
-//  void dispose() {
-//    super.dispose();
-//    controller.close();
-//  }
-//
-//  @override
-//  void initState() {
-//    super.initState();
-//    _platformChannel2
-//        .receiveBroadcastStream()
-//        .listen(_onEvent, onError: _onError);
-//  }
-//
-//  void _onEvent(Object event) {
-//    print(event);
-//    controller.sink.add("$event");
-//  }
-//
-//  void _onError(Object error) {
-//    controller.sink.add("Error");
-//  }
-//
-//  @override
-//  Widget build(BuildContext context) {
-//    return ScopedModel<AppModel>(
-//      model: AppModel(),
-//child: ScopedModelDescendant<Model>(
-//  builder: (context, child, model) => Card(
-//    color: Color.fromARGB(255, 76, 175, 80),
-//    elevation: 5.0,
-//    child: InkWell(
-//      splashColor: Colors.blueGrey,
-//
-//      child: Center(
-//        child: Column(
-//          mainAxisAlignment: MainAxisAlignment.center,
-//          crossAxisAlignment: CrossAxisAlignment.center,
-////            children: <Widget>[
-////              Padding(
-////                padding: EdgeInsets.only(left: 10.0, right: 10.0),
-////                child: StreamBuilder<String>(
-////                  stream: _statusStream,
-////                  builder: (context, snapshot) {
-////                    if (snapshot.hasData) {}
-////                    return Text(
-////                      "${snapshot.data}",
-////                      style: TextStyle(color: Colors.white, fontSize: 20.0),
-////                      textAlign: TextAlign.center,
-////                    );
-////                  },
-////                ),
-////              ),
-////            ],
-////        ),
-////      ),
-////,
-////  ),
-////),
-////
-////    );
-////  }
-//
-//
-////      child: Card(
-////        color: Color.fromARGB(255, 76, 175, 80),
-////        elevation: 5.0,
-////        child: InkWell(
-////          splashColor: Colors.blueGrey,
-////          onTap: () {
-////            setState(
-////              () {
-////                pressed = !pressed;
-////              },
-////            );
-////            ScopedModelDescendant<AppModel>(
-////              builder: (context, child, model) {
-////                pressed ? model.dndOn() : model.dndOff();
-////              },
-////            );
-////          },
-////          child: Center(
-////            child: Column(
-////              mainAxisAlignment: MainAxisAlignment.center,
-////              crossAxisAlignment: CrossAxisAlignment.center,
-////              children: <Widget>[
-////                Padding(
-////                  padding: EdgeInsets.only(left: 10.0, right: 10.0),
-////                  child: StreamBuilder<String>(
-////                    stream: _statusStream,
-////                    builder: (context, snapshot) {
-////                      if (snapshot.hasData) {}
-////                      return Text(
-////                        "${snapshot.data}",
-////                        style: TextStyle(color: Colors.white, fontSize: 20.0),
-////                        textAlign: TextAlign.center,
-////                      );
-////                    },
-////                  ),
-////                ),
-////              ],
-////            ),
-////          ),
-////        ),
-////      ),
--- lib/model.dart
+++ lib/model.dart
@@ -1,10 +1,11 @@
+import 'dart:async';
+
 import 'package:flutter/services.dart';
 import 'package:scoped_model/scoped_model.dart';
 
-
 class AppModel extends Model {
   final _platformChannel1 =
-  MethodChannel('strangerweather.com/easy_dnd/receiver');
+      MethodChannel('strangerweather.com/easy_dnd/receiver');
 
   Future<Null> dndOn() async {
     await _platformChannel1.invokeMethod('ON');
