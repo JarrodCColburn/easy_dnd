60583d3f653096145a348c7604021572d06b95a6
strangerweather <strangerweather@gmail.com>
2019-01-04 08:32:00 +0000

    designed basic settings screen

--- lib/cards/settings_card_1.dart
+++ lib/cards/settings_card_1.dart
@@ -1,18 +1,55 @@
 import 'package:flutter/material.dart';
 
-class SettingsCardOne extends StatefulWidget{
-  @override
-  State<StatefulWidget> createState() {
 
-    return SettingsCardOneState();
-  }
-
-}
-
-class SettingsCardOneState extends State <SettingsCardOne>{
+class SettingsCardOne extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
 
-    return null;
+    return Container(
+      margin: const EdgeInsets.symmetric(
+        vertical: 16.0,
+        horizontal: 24.0,
+      ),
+      child: Stack(
+        children: <Widget>[
+          settingsCard,
+          settingsThumbnail,
+        ],
+      ),
+    );
   }
 }
+
+
+final settingsThumbnail = Container(
+margin: EdgeInsets.symmetric(
+  vertical: 16.0),
+  alignment: FractionalOffset.centerLeft,
+  child: settingsCircle,
+);
+
+final settingsCircle = Container(
+  width: 92.0,
+  height: 92.0,
+  decoration: new BoxDecoration(
+    color: Colors.orange,
+    shape: BoxShape.circle,
+  ),
+);
+
+final settingsCard = Container(
+  height: 124.0,
+  margin: EdgeInsets.only(left: 46.0),
+  decoration: BoxDecoration(
+    color: Color.fromARGB(255, 76, 175, 80),
+    shape: BoxShape.rectangle,
+    borderRadius: BorderRadius.circular(8.0),
+    boxShadow: <BoxShadow>[
+      BoxShadow(
+        color: Colors.black12,
+        blurRadius: 10,
+        offset: Offset(0.0, 10.0)
+      )
+    ]
+  ),
+);
--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -1,7 +1,3 @@
-import 'package:easy_dnd/cards/card_1.dart';
-import 'package:easy_dnd/cards/card_2.dart';
-import 'package:easy_dnd/cards/card_3.dart';
-import 'package:easy_dnd/cards/card_4.dart';
 import 'package:easy_dnd/cards/settings_card_1.dart';
 import 'package:easy_dnd/cards/settings_card_2.dart';
 import 'package:easy_dnd/cards/settings_card_3.dart';
@@ -9,26 +5,16 @@ import 'package:easy_dnd/cards/settings_card_4.dart';
 import 'package:easy_dnd/cards/settings_card_5.dart';
 import 'package:flutter/material.dart';
 
-class SettingsPage extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    return SettingsPageState();
-  }
-}
 
-class SettingsPageState extends State<SettingsPage> {
+
+class SettingsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Easy DND'),
       ),
-      body: GridView.count(
-        crossAxisCount: 1,
-        childAspectRatio: 1.0,
-        padding: const EdgeInsets.all(15.0),
-        mainAxisSpacing: 4.0,
-        crossAxisSpacing: 4.0,
+      body: ListView(
         children: <Widget>[
           SettingsCardOne(),
           SettingsCardTwo(),
